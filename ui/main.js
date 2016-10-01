9a28409b3e5ea9a3e3fb0d210b09e064ba594bdc
borne96 <sistla_meena@yahoo.com>
2016-10-01 13:47:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
- marginLeft=marginLeft+5;
- img.style.marginLeft=marginLeft+'px';
+  marginLeft=marginLeft+5;
+ img.style.marginLeft= marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight(),100);
+    var interval=setInterval(moveRight,100);
     //img.style.marginLeft='100px';
 };
