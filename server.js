72fa8d07a5e35b194eabb6c64d0c3b7b62a9ee49
borne96 <sistla_meena@yahoo.com>
2016-10-01 23:37:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var counter=0;
 res.send(counter.toString());
 app.get('/counter',function(req,res)
 {
+    var counter=0;
     counter=counter+1;
     res.send(counter.toString());
     
