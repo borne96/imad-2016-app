ae6e59582998945aebe7138b3b1492bfa83a0983
borne96 <sistla_meena@yahoo.com>
2016-10-01 12:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req,res){
+   res.send('See article one here'); 
+});
+app.get('/article2',function(req,res){
+   res.send('See article two here'); 
+});
+app.get('/article3',function(req,res){
+   res.send('See article three here'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
